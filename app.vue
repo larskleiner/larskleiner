b35c195b1adaca5976900740bf277030e240cb35
Kleiner Lars <larskleiner@googlemail.com>
2023-11-30 12:46:47 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { locale } = useI18n()
+</script>
+
+<template>
+  <div>
+    <form>
+      <select v-model="locale">
+        <option value="en">en</option>
+        <option value="de">de</option>
+      </select>
+      <p>{{ $t('welcome') }}</p>
+    </form>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    [
+      '@nuxtjs/i18n',
+      {
+        vueI18n: './i18n.config.ts'
+      }
+    ]
+  ],
+  css: ['~/assets/styles/main.scss']
+});
