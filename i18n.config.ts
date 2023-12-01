b5929d33822f465adacfcdf25c9f69ec6c84b702
Kleiner Lars <larskleiner@googlemail.com>
2023-12-01 19:16:56 +0100

    Layout & styling

--- i18n.config.ts
+++ i18n.config.ts
@@ -3,10 +3,14 @@ export default defineI18nConfig(() => ({
     locale: 'de',
     messages: {
         en: {
-            hello: 'Hello, I\'m Lars Kleiner. Web developer, Drupal expert, cyclist and amateur photographer.'
+            hello: 'Hello, I\'m Lars Kleiner. Web developer, Drupal expert, cyclist and amateur photographer.',
+            linkedin: 'linkedin blurb here.',
+            strava: 'strava blurb here.',
         },
         de: {
-            hello: 'Hallo, ich bin Lars Kleiner. Webentwickler, Drupal-Experte, Radfahrer und Hobbyfotograf.'
+            hello: 'Hallo, ich bin Lars Kleiner. Webentwickler, Drupal-Experte, Radfahrer und Hobbyfotograf.',
+            linkedin: 'linkedin blurb here.',
+            strava: 'strava blurb here.',
         }
     }
 }))
--- pages/index.vue
+++ pages/index.vue
@@ -12,20 +12,38 @@ const availableLocales = computed(() => {
 </script>
 
 <template>
-  <div class="container-fluid">
+  <div>
     <nav>
       <template v-for="(locale, index) in availableLocales" :key="locale.code">
         <template v-if="index"> | </template>
-        <NuxtLink class="btn btn-primary" :to="switchLocalePath(locale.code)">{{ locale.name ?? locale.code }}</NuxtLink>
+        <NuxtLink class="btn btn-primary btn-dark mb-3" :to="switchLocalePath(locale.code)">{{ locale.name ?? locale.code }}</NuxtLink>
       </template>
     </nav>
     <article>
-      <div class="row">
-        <div class="col pt-3">
-          <NuxtImg preload src="/img/larskleiner.png" class="border border-secondary rounded-3" alt="Lars Kleiner" />
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-1 pt-md-3">
+            <NuxtImg preload src="/img/larskleiner.png" class="float-md-end border border-secondary rounded-3" alt="Lars Kleiner" />
+          </div>
+          <div class="col-11">
+            {{ t('hello') }}
+          </div>
         </div>
-        <div class="col-11">
-          {{ t('hello') }}
+        <div class="row mt-3">
+          <div class="col-md-1 pt-md-3">
+            <NuxtLink to="https://www.linkedin.com/in/larskleiner"><NuxtImg preload src="/img/linkedin.png" class="float-md-end" alt="linkedin" /></NuxtLink>
+          </div>
+          <div class="col-11">
+            {{ t('linkedin') }}
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col-md-1 pt-md-3">
+            <NuxtLink to="https://www.strava.com/athletes/2479560"><NuxtImg preload src="/img/strava.png" class="float-md-end" alt="strava" /></NuxtLink>
+          </div>
+          <div class="col-11">
+            {{ t('strava') }}
+          </div>
         </div>
       </div>
     </article>
