69f080ecfef603b3fc83ee983653b1067a9caf29
Kleiner Lars <larskleiner@googlemail.com>
2023-12-01 09:28:50 +0100

    Get i18n to work with browser language detection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,14 @@ export default defineNuxtConfig({
     [
       '@nuxtjs/i18n',
       {
+        locales: ['en', 'de'],
+        defaultLocale: 'de',
+        detectBrowserLanguage: {
+          useCookie: true,
+          cookieKey: "i18n_redirected",
+          alwaysRedirect: true,
+          onlyOnRoot: true,
+        },
         vueI18n: './i18n.config.ts'
       }
     ]
