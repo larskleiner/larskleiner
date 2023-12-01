7aabf62552e50a5ca447f12fa3753682f7d41c42
Kleiner Lars <larskleiner@googlemail.com>
2023-12-01 19:28:57 +0100

    More styling

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@ const availableLocales = computed(() => {
     </nav>
     <article>
       <div class="container-fluid">
-        <div class="row">
+        <div class="row larskleiner">
           <div class="col-md-1 pt-md-3">
             <NuxtImg preload src="/img/larskleiner.png" class="float-md-end border border-secondary rounded-3" alt="Lars Kleiner" />
           </div>
@@ -30,16 +30,16 @@ const availableLocales = computed(() => {
           </div>
         </div>
         <div class="row mt-3">
-          <div class="col-md-1 pt-md-3">
-            <NuxtLink to="https://www.linkedin.com/in/larskleiner"><NuxtImg preload src="/img/linkedin.png" class="float-md-end" alt="linkedin" /></NuxtLink>
+          <div class="col-md-1 pt-md-2">
+            <NuxtLink to="https://www.linkedin.com/in/larskleiner" class="float-md-end"><NuxtImg preload src="/img/linkedin.png" alt="linkedin" /></NuxtLink>
           </div>
           <div class="col-11">
             {{ t('linkedin') }}
           </div>
         </div>
         <div class="row mt-3">
-          <div class="col-md-1 pt-md-3">
-            <NuxtLink to="https://www.strava.com/athletes/2479560"><NuxtImg preload src="/img/strava.png" class="float-md-end" alt="strava" /></NuxtLink>
+          <div class="col-md-1 pt-md-2">
+            <NuxtLink to="https://www.strava.com/athletes/2479560" class="float-md-end"><NuxtImg preload src="/img/strava.png" alt="strava" /></NuxtLink>
           </div>
           <div class="col-11">
             {{ t('strava') }}
