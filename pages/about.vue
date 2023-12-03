253bb60edf174f7eaa1b5b297fa251cbce467cd0
Kleiner Lars <larskleiner@googlemail.com>
2023-12-03 11:42:04 +0100

    Privacy & about copy

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,17 @@
 <template>
   <article>
     <div class="container-fluid">
-      <div class="row justify-content-center">
-        Lars Kleiner
-      </div>
-      <div class="row justify-content-center">
-        Bahrenfelder Steindamm 109c
-      </div>
-      <div class="row justify-content-center">
-        22761 Hamburg
-      </div>
-      <div class="row justify-content-center">
-        email: hello@larskleiner.de
+      <div class="row">
+        <h2>Angaben gemäß § 5 Telemediengesetz</h2>
+        Lars Kleiner<br />
+        Bahrenfelder Steindamm 109c<br />
+        22761 Hamburg<br />
+        Telefon: 01737798303<br />
+        E-Mail: hello@larskleiner.de<br />
+        Umsatzsteuer ID: DE280194949<br />
       </div>
     </div>
   </article>
 </template>
+<script setup lang="ts">
+</script>
