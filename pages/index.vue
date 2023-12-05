2b59206ed16ce49b20a4f08fc90176ca7137d39d
Kleiner Lars <larskleiner@googlemail.com>
2023-12-05 17:13:44 +0100

    Copy formatting

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@ const availableLocales = computed(() => {
     <article class="home pb-5">
       <div class="container-fluid">
         <div class="row larskleiner">
-          <div class="col-md-1 pt-md-3">
+          <div class="col-md-1 d-flex flex-column">
             <NuxtImg preload src="/img/larskleiner.png" class="float-md-end border border-secondary rounded-3" alt="Lars Kleiner" />
           </div>
           <div class="col-11">
@@ -25,7 +25,7 @@ const availableLocales = computed(() => {
           </div>
         </div>
         <div class="row mt-3">
-          <div class="col-md-1 pt-md-2">
+          <div class="col-md-1 d-flex flex-column">
             <NuxtLink to="https://www.linkedin.com/in/larskleiner" class="float-md-end"><NuxtImg preload src="/img/linkedin.png" alt="linkedin" /></NuxtLink>
           </div>
           <div class="col-11">
@@ -33,7 +33,7 @@ const availableLocales = computed(() => {
           </div>
         </div>
         <div class="row mt-3">
-          <div class="col-md-1 pt-md-2">
+          <div class="col-md-1 d-flex flex-column">
             <NuxtLink to="https://www.strava.com/athletes/2479560" class="float-md-end"><NuxtImg preload src="/img/strava.png" alt="strava" /></NuxtLink>
           </div>
           <div class="col-11">
